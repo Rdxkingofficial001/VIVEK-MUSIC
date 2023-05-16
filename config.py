9b8a5acc1d90eb0d576ad27cb5ecbab99a066a0f
Rdxkingofficial001 <132626493+Rdxkingofficial001@users.noreply.github.com>
2023-05-17 00:11:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rdxkingofficial001/VIVEK-MUSIC/edit/master/config.py",
+    "https://github.com/Rdxkingofficial001/VIVEK-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
